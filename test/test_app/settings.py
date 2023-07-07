0af1b5577a38bab73f28790d6188e275c6e47fad
Tom Jeannesson <tomjeannesson@gmail.com>
2023-07-08 00:10:58 +0200

    test

--- test/test_app/settings.py
+++ test/test_app/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # django-napse
+    #  django-napse
     "django_napse",
     "django_napse.napse_core",
     "django_napse.napse_simulations",
