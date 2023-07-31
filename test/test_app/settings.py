db40cfedcea9130253bd6a585d2e4471f44b5134
Tom Jeannesson <tomjeannesson@gmail.com>
2023-07-31 14:53:48 +0200

    fix(node): max old space 1400

--- test/test_app/settings.py
+++ test/test_app/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = "wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "neondb",
+        "USER": "napse.invest",
+        "PASSWORD": "JkXZv8lc5DsQ",
+        "HOST": "ep-tight-night-26558438.eu-central-1.aws.neon.tech",
+        "PORT": "5432",
     },
 }
 
