706c1f6226debd04468005e371add80c6ac45e98
Tom Jeannesson <tomjeannesson@gmail.com>
2023-08-07 14:54:05 +0200

    fix(ci): secrets in pipelines

---
+++ test/test_app/setup_secrets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+    export $(xargs < .env)
+fi
+
+cat << EOF > secret.json
+{
+    "Exchange Accounts": {
+        "BINANCE Test Account": {
+            "exchange": "BINANCE",
+            "testing": true,
+            "public_key": "$BINANCE_PUBLIC_KEY",
+            "private_key": "$BINANCE_PRIVATE_KEY"
+        }
+    }
+}
+EOF
