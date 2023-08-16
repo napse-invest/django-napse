75aa23aa2f0aeb73feb9ada19ebfa047d37b8ff1
Xenepix38 <nico.faglin@gmail.com>
2023-08-16 19:46:30 +0200

    tmp

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from distutils.core import setup
 
+# from setuptools import setup
+
 setup(
     name="django-napse",  # How you named your package folder (MyLib)
-    packages=["django-napse"],  # Chose the same as "name"
+    packages=["django_napse"],  # Chose the same as "name"
     version="1.4.0",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="The heart and brains of the Napse Invest platform.",  # Give a short description about your library
     author="Napse Invest",  # Type in your name
     author_email="napse.invest@gmail.com",  # Type in your E-Mail
     url="https://github.com/napse-invest/django-napse",  # Provide either the link to your github or to your website
-    download_url="https://github.com/napse-invest/django-napse/archive/v_01.tar.gz",  # I explain this later on
+    download_url="https://github.com/napse-invest/django-napse/archive/refs/tags/v1.4.0.tar.gz",
     keywords=["Investing", "Django", "Trading"],  # Keywords that define your package best
-    install_requires=[  # I get to this in a second
+    install_requires=[
         "django",
         "django-environ",
         "django-celery-beat",
