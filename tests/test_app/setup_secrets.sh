5713ca2b1464c2dd46e293709dea5c97d550e804
Tom JEANNESSON <tomjeannesson@wificampus-096164.grenet.fr>
2023-11-21 18:34:54 +0100

    fix(accessibility): added ease of use tools for the secrets

--- tests/test_app/setup_secrets.sh
+++ tests/test_app/setup_secrets.sh
@@ -3,17 +3,31 @@
 
 if [ -f .env ]; then
     export $(xargs < .env)
+else
+    cat << EOF > .env
+BINANCE_PUBLIC_KEY=
+BINANCE_PRIVATE_KEY=
+EOF
+    echo "Please fill in the .env file with your API keys."
+fi
+if [ ! -f secrets.json ]; then
+    cat << EOF > secrets.json
+{
+    "Exchange Accounts": {}
+}
+EOF
 fi
 
-cat << EOF > secrets.json
-{
-    "Exchange Accounts": {
-        "BINANCE Test Account": {
-            "exchange": "BINANCE",
-            "testing": true,
-            "public_key": "$BINANCE_PUBLIC_KEY",
-            "private_key": "$BINANCE_PRIVATE_KEY"
-        }
+python3 << EOF
+import json
+with open('secrets.json', 'r') as f:
+    secrets = json.load(f)
+    secrets['Exchange Accounts']['BINANCE Test Account'] = {
+        'exchange': 'BINANCE',
+        'testing': True,
+        'public_key': '$BINANCE_PUBLIC_KEY',
+        'private_key': '$BINANCE_PRIVATE_KEY'
     }
-}
+with open('secrets.json', 'w') as f:
+    json.dump(secrets, f, indent=4)
 EOF
