66bbba693fc0b40787ac3f3c343c3c3fe83973f6
Xenepix <Firenix.nex@gmail.com>
2024-01-07 22:45:53 +0100

    fix(bot): add space containerization

--- django_napse/api/bots/serializers/bot_serializers.py
+++ django_napse/api/bots/serializers/bot_serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import empty
 
 from django_napse.api.bots.serializers.strategy_serializer import StrategySerializer
 from django_napse.core.models import Bot, BotHistory
@@ -7,6 +8,7 @@ from django_napse.core.models import Bot, BotHistory
 class BotSerializer(serializers.ModelSerializer):
     strategy = StrategySerializer()
     delta = serializers.SerializerMethodField(read_only=True)
+    space = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Bot
@@ -16,12 +18,22 @@ class BotSerializer(serializers.ModelSerializer):
             "strategy",
             "value",
             "delta",
+            "fleet",
+            "space",
+            "exchange_account",
         ]
         read_only_fields = [
             "uuid",
             "value",
             "delta",
+            "fleet",
+            "space",
+            "exchange_account",
         ]
+
+    def __init__(self, instance=None, data=empty, space=None, **kwargs):
+        self.space = space
+        super().__init__(instance=instance, data=data, **kwargs)
 
     def get_delta(self, instance) -> float:
         """Delta on the last 30 days."""
@@ -30,3 +42,14 @@ class BotSerializer(serializers.ModelSerializer):
         except BotHistory.DoesNotExist:
             return 0
         return history.get_delta()
+
+    def get_space(self, instance):
+        if self.space is None:
+            return None
+        return self.space.uuid
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["fleet"] = representation["fleet"].uuid
+        representation["exchange_account"] = representation["exchange_account"].uuid
+        return representation
--- django_napse/api/bots/views/bot_view.py
+++ django_napse/api/bots/views/bot_view.py
@@ -5,7 +5,7 @@ from rest_framework_api_key.permissions import HasAPIKey
 from django_napse.api.bots.serializers.bot_serializers import BotSerializer
 from django_napse.api.custom_permissions import HasSpace
 from django_napse.api.custom_viewset import CustomViewSet
-from django_napse.core.models import Bot
+from django_napse.core.models import Bot, NapseSpace
 
 
 class BotView(CustomViewSet):
@@ -29,7 +29,50 @@ class BotView(CustomViewSet):
             case _:
                 return super().get_permissions()
 
+    def _get_boolean_query_param(self, param: str) -> bool | None:
+        """Return None if a boolean cannot be found."""
+        if isinstance(param, bool):
+            return param
+
+        if not isinstance(param, str):
+            return None
+
+        match param.lower():
+            case "true" | "1":
+                return True
+            case "false" | "0":
+                return False
+            case _:
+                return None
+
     def list(self, request):
+        space_containers = self._get_boolean_query_param(request.query_params.get("space_containers", True))
+        space_uuid = request.query_params.get("space", None)
+        api_key = self.get_api_key(request)
+
+        if not space_containers and api_key.is_master_key:
+            # Not space_containers mode is only available for master key
+            serializer = self.get_serializer(self.get_queryset(), many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # Get spaces from API key
+        spaces = NapseSpace.objects.all() if api_key.is_master_key else [permission.space for permission in api_key.permissions.all()]
+        # Filter by specific space
+        if space_uuid is not None:
+            space = NapseSpace.objects.get(uuid=space_uuid)
+            if space not in spaces:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            spaces = [space]
+
+        # Bot list
+        bots = []
+        for space in spaces:
+            serializer = self.get_serializer(space.bots, many=True, space=space)
+            if serializer.data != []:
+                bots += serializer.data
+        return Response(bots, status=status.HTTP_200_OK)
+
+        # TODO: make space containerization
         serializer = self.get_serializer(self.get_queryset(), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
