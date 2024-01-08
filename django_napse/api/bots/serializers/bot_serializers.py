acbecfecde320048c90c48dc98372039a0ea4202
Xenepix <Firenix.nex@gmail.com>
2024-01-08 20:04:30 +0100

    add base of BotDetailSerializer

--- django_napse/api/bots/serializers/bot_serializers.py
+++ django_napse/api/bots/serializers/bot_serializers.py
@@ -1,21 +1,22 @@
 from rest_framework import serializers
 from rest_framework.fields import empty
 
-from django_napse.api.bots.serializers.strategy_serializer import StrategySerializer
-from django_napse.core.models import Bot, BotHistory
+from django_napse.api.wallets.serializers import WalletSerializer
+from django_napse.core.models import Bot, BotHistory, ConnectionWallet
 
 
 class BotSerializer(serializers.ModelSerializer):
-    strategy = StrategySerializer()
+    # strategy = StrategySerializer()
     delta = serializers.SerializerMethodField(read_only=True)
     space = serializers.SerializerMethodField(read_only=True)
+    exchange_account = serializers.CharField(source="exchange_account.uuid", read_only=True)
+    fleet = serializers.CharField(source="fleet.uuid", read_only=True)
 
     class Meta:
         model = Bot
         fields = [
             "name",
             "uuid",
-            "strategy",
             "value",
             "delta",
             "fleet",
@@ -26,9 +27,7 @@ class BotSerializer(serializers.ModelSerializer):
             "uuid",
             "value",
             "delta",
-            "fleet",
             "space",
-            "exchange_account",
         ]
 
     def __init__(self, instance=None, data=empty, space=None, **kwargs):
@@ -48,8 +47,87 @@ class BotSerializer(serializers.ModelSerializer):
             return None
         return self.space.uuid
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation["fleet"] = representation["fleet"].uuid
-        representation["exchange_account"] = representation["exchange_account"].uuid
-        return representation
+
+class BotDetailSerializer(serializers.ModelSerializer):
+    delta = serializers.SerializerMethodField(read_only=True)
+    space = serializers.SerializerMethodField(read_only=True)
+    exchange_account = serializers.CharField(source="exchange_account.uuid", read_only=True)
+    fleet = serializers.CharField(source="fleet.uuid", read_only=True)
+
+    statistics = serializers.SerializerMethodField(read_only=True)
+    wallet = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Bot
+        fields = [
+            "name",
+            "uuid",
+            "value",
+            "statistics",
+            "wallet",
+            "fleet",
+            "space",
+            "exchange_account",
+        ]
+        read_only_fields = [
+            "uuid",
+            "value",
+            "statistics",
+            "wallet",
+            "space",
+        ]
+
+    def __init__(self, instance=None, data=empty, space=None, **kwargs):
+        self.space = space
+        super().__init__(instance=instance, data=data, **kwargs)
+
+    def get_delta(self, instance) -> float:
+        """Delta on the last 30 days."""
+        try:
+            history = BotHistory.objects.get(owner=instance)
+        except BotHistory.DoesNotExist:
+            return 0
+        return history.get_delta()
+
+    def get_space(self, instance):
+        if self.space is None:
+            return None
+        return self.space.uuid
+
+    def get_wallet(self, instance):
+        def _search_ticker(ticker: str, merged_wallet) -> int | None:
+            """Return the index of the currency in the list if found, None otherwise."""
+            for i, currency in enumerate(merged_wallet):
+                if currency.get("ticker").ticker == ticker:
+                    return i
+            return None
+
+        def _update_merged_wallet(index: int, currency: str, merged_wallet) -> None:
+            """Update the merged wallet with the new currency."""
+            if index is None:
+                merged_wallet.append(
+                    {
+                        "ticker": currency.ticker,
+                        "amount": currency.amount,
+                        "mbp": currency.mbp,
+                    },
+                )
+            else:
+                merged_wallet[index]["amount"] += currency.amount
+
+        if self.space is not None:
+            wallet = ConnectionWallet.objects.get(
+                owner__owner=self.space.wallet,
+                owner__bot=instance,
+            )
+            return WalletSerializer(wallet).data
+
+        wallets = [connection.wallet for connection in instance.connections.all()]
+        merged_wallet: list[dict[str, str | float]] = []
+
+        for wallet in wallets:
+            for currency in wallet.currencies.all():
+                index = _search_ticker(currency.ticker, merged_wallet)
+                _update_merged_wallet(index, currency, merged_wallet)
+
+        return merged_wallet
