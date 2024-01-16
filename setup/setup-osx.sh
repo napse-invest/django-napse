bd64ecfecb4069e18b9a6e9796c684c4ba2ca55d
Tom JEANNESSON <tomjeannesson@wificampus-096164.grenet.fr>
2024-01-16 20:35:20 +0100

    tmp

--- setup/setup-osx.sh
+++ setup/setup-osx.sh
@@ -1,9 +1,9 @@
 brew install python@3.11
 pip3 install virtualenv
-pip3 install pip-tools
 python3 -m virtualenv .venv --python=python3.11
 printf "\n===============================================\nVirtual python environment has been created.\n"
 source .venv/bin/activate
+pip3 install pip-tools
 printf "Virtual python environment has been activated.\n"
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
 printf "Compiling requirements... This may take a few minutes.\n"
