163fa52dab29c0e0478d3ce5ea4cff5246a4a1ca
Xenepix <Firenix.nex@gmail.com>
2024-02-27 22:38:30 +0100

    refactor(ruff): compliance

--- django_napse/utils/serializers/fields.py
+++ django_napse/utils/serializers/fields.py
@@ -2,32 +2,49 @@ import uuid
 from datetime import datetime
 
 
-def instance_check(target_type) -> callable:
-    def _instance_check(instance, target_type=target_type):
+def instance_check(target_type: any) -> callable:
+    """Produce a callable to check if an instance is of a specific type."""
+
+    def _instance_check(instance: object, target_type: any = target_type) -> bool:
         return isinstance(instance, target_type)
 
     return _instance_check
 
 
 class Field:
+    """Fields represent models' fields in the serializer."""
+
     validate = None
     # Define if the getter method takes the serializer as argument.
     getter_takes_serializer = False
 
-    def __init__(self, required: bool = False, source: str | None = None):
+    def __init__(self, required: bool = False, source: str | None = None) -> None:  # noqa: FBT001, FBT002
+        """Define basic parameters of the field.
+
+        Parameters:
+            required: bool
+                Define if the field is required.
+            source: str
+                Define the source of the field in the model (ex: `exchange.name`).
+        """
         self.required = required
         self.source = source
 
-    def to_value(self, value):
+    def to_value(self, value: any) -> any:
         """Overwrite this method for custom transformation on the serialized value."""
         return value
 
-    def as_getter(self, serializer_field_name, serializer_cls) -> None:
+    def as_getter(self, serializer_field_name: str, serializer_cls: type) -> None:  # noqa: ARG002
         """Return a getter method for the field."""
         return
 
 
 class StrField(Field):
+    """Represent a string.
+
+    Can be used for model.CharField, model.TextField, ...
+    """
+
     to_value: callable = staticmethod(str)
     validate: callable = staticmethod(instance_check(str))
 
